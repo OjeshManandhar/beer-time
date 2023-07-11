f473d1179957fafa5f3573e28816bf6ad7a44cee
OjeshManandhar <ozes.manandhar@gmail.com>
2023-07-11 19:08:41 +0545

    Absolute imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+// components
+import Test from '@/components/Test';
+import Dummy from '@/components/Dummy';
+
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>Beer Time</h1>
+
+      <Test />
+      <Dummy />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Dummy/index.tsx
@@ -0,0 +1,17 @@
+// components
+import Test from '@/components/Test';
+
+function Dummy() {
+  return (
+    <>
+      <h1>Dummy</h1>
+      <strong>
+        <em>
+          <Test />
+        </em>
+      </strong>
+    </>
+  );
+}
+
+export default Dummy;
