8f5d6103bce0743661a0bc1ac89238506351e703
OjeshManandhar <ozes.manandhar@gmail.com>
2023-07-12 12:06:02 +0545

    Move Tooltip inside li

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -6,35 +6,33 @@ import type { Props } from './types';
 
 function Card({ beer }: Props) {
   return (
-    <>
-      <li className='flex flex-row h-40 gap-5 p-6 overflow-hidden bg-white rounded cursor-pointer drop-shadow-md hover:bg-alice-blue'>
-        <img
-          id={`all-beer-img-${beer.id}`}
-          src={beer.image_url}
-          alt={beer.name}
-          className='hidden h-full min-w-[75px] max-w-[75px] object-contain sm:block'
-        />
+    <li className='flex flex-row h-40 gap-5 p-6 overflow-visible bg-white rounded cursor-pointer drop-shadow-md hover:bg-alice-blue'>
+      <img
+        id={`all-beer-img-${beer.id}`}
+        src={beer.image_url}
+        alt={beer.name}
+        className='hidden object-contain h-full sm:block min-w-[75px] max-w-[75px]'
+      />
 
-        <div>
-          <p className='text-2xl font-semibold line-clamp-1'>{beer.name}</p>
-          <p className='mt-2 text-sm font-semibold text-lemon-curry line-clamp-1'>
-            {beer.tagline}
-          </p>
-          <p className='mt-2 text-sm line-clamp-2'>{beer.description}</p>
-        </div>
-      </li>
+      <div>
+        <p className='text-2xl font-semibold line-clamp-1'>{beer.name}</p>
+        <p className='mt-2 text-sm font-semibold text-lemon-curry line-clamp-1'>
+          {beer.tagline}
+        </p>
+        <p className='mt-2 text-sm line-clamp-2'>{beer.description}</p>
+      </div>
 
       <Tooltip
         anchorSelect={`#all-beer-img-${beer.id}`}
         delayShow={500}
         place='top'
-        className='max-w-[250px] px-2 py-1 bg-raisin-black'
+        className='px-2 py-1 bg-raisin-black max-w-[250px]'
       >
         {typeof beer.ingredients === 'string'
           ? beer.ingredients
           : beer.ingredients.join(', ')}
       </Tooltip>
-    </>
+    </li>
   );
 }
 
